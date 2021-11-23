9760458a5bae1be5916c81c62eec9d1d694afe33
dimma111 <marinenko96@yandex.ru>
2021-11-23 17:11:35 +0500

    add moment lib

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,34 @@ import LaunchCard from "../components/LaunchCard";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import { styled } from "@mui/material/styles";
+import launchesStore from "../store/mainStore";
+import { observer } from "mobx-react-lite";
 
 import styles from "../styles/Home.module.scss";
+import { useEffect } from "react";
 
-const apiAllLaunches = "https://api.spacexdata.com/v5/launches/query";
-let countLaunches = 12;
+const Home = observer(() => {
+  let currentPage = 1;
 
-const Home = ({ launches }) => {
-  const cards = launches.docs.map((anObjectMapped, index) => {
-    return (
-      <Grid key={index} item xs={12} sm={6} md={4}>
-        <LaunchCard data={anObjectMapped} />
-      </Grid>
-    );
-  });
+  useEffect(() => {
+    launchesStore.fetchLaunches(currentPage);
+    document.addEventListener("scroll", scrollHandler);
+    return function () {
+      document.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+
+  const scrollHandler = (e) => {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+      1
+    ) {
+      currentPage++;
+      launchesStore.fetchLaunches(currentPage);
+      console.log("endscroll");
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -32,38 +46,45 @@ const Home = ({ launches }) => {
       </header>
 
       <main>
-        <div className="launches">
+        <div className={styles.launches}>
           <Container>
             <Grid container spacing={2}>
-              {cards}
+              {launchesStore.launches.map((anObjectMapped, index) => {
+                return (
+                  <Grid key={index} item xs={12} sm={6} md={4}>
+                    <LaunchCard data={anObjectMapped} />
+                  </Grid>
+                );
+              })}
             </Grid>
           </Container>
         </div>
       </main>
     </div>
   );
-};
+});
 
-export async function getStaticProps() {
-  const requestOptions = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      options: {
-        limit: 12,
-        page: 2,
-      },
-    }),
-  };
+// export async function getStaticProps() {
+//   const requestOptions = {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({
+//       options: {
+//         limit: 12,
+//         page: 2,
+//       },
+//     }),
+//   };
 
-  const res = await fetch(apiAllLaunches, requestOptions);
-  const launches = await res.json();
+//   const res = await fetch(apiAllLaunches, requestOptions);
+//   const launches = await res.json();
 
-  return {
-    props: {
-      launches,
-    },
-  };
-}
+//   launchesStore.addLaunch(launches.docs);
+//   return {
+//     props: {
+//       launches,
+//     },
+//   };
+// }
 
 export default Home;
